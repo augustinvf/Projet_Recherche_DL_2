ac3b1d4e4dd68b170f0cf4254247b1607fe97f5a
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-13 11:39:25 +0200

    argument current_phase correction

--- model.py
+++ model.py
@@ -3,13 +3,12 @@ import torchvision
 import torch.nn as nn
 
 class Model(nn.Module):
-    def __init__(self, projection_head, classifier, current_phase):
+    def __init__(self, projection_head, classifier):
         super().__init__()
         resnet = torchvision.models.resnet18()
         self.backbone = nn.Sequential(*list(resnet.children())[:-1])
         self.projection_head = projection_head
         self.classifier = classifier
-        self.current_phase = current_phase
 
     def forward(self, x, current_phase):
         if current_phase == "self_supervised" :
