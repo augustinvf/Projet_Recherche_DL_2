68f71eda783229cae8a45880995bf887479e6569
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-17 11:30:58 +0200

    test for run

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ for epochs in range(nb_epochs) :
         optimizer_su.step()
         scheduler_su.step()
 
-    wandb.log({"loss self-supervised": loss_ss, 
+        wandb.log({"loss self-supervised": loss_ss, 
                "loss supervised": loss_su, 
                "accuracy": accuracy, 
                "scheduler self-supervised": scheduler_ss.get_lr()[epochs], 
