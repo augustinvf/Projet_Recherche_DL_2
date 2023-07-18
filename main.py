954db7c535322f0a33707e3e7c5b2322c953a6fd
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-18 15:32:12 +0200

    debug

--- main.py
+++ main.py
@@ -25,25 +25,24 @@ nb_classes = 10
 input_size_classifier = 512
 projection_head = SimCLRProjectionHead(512, 512, 128)
 nb_steps = len(train_dataloader_supervised)
-nb_epochs = 100
+
+nb_cycles = 1
+nb_epochs_self_supervised = 100
+nb_epochs_supervised = 100
 
 model = Model(projection_head, input_size_classifier, nb_classes).to(device)
 
 criterion_ss = NTXentLoss()
 optimizer_ss = torch.optim.Adam(list(model.backbone.parameters()) + list(model.projection_head.parameters()), 0.0003, weight_decay=1e-4)
-scheduler_ss = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_ss, T_max=nb_epochs, eta_min=0,
+scheduler_ss = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_ss, T_max=nb_cycles*nb_epochs_self_supervised, eta_min=0,
                                                            last_epoch=-1)
 
 criterion_su = nn.CrossEntropyLoss()
-optimizer_su = torch.optim.Adam(model.classifier.parameters(), 0.001, weight_decay=1e-5)
-scheduler_su = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_su, T_max=nb_epochs, eta_min=0,
+optimizer_su = torch.optim.Adam(model.classifier.parameters(), 0.0003, weight_decay=1e-5)
+scheduler_su = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_su, T_max=nb_cycles*nb_epochs_supervised, eta_min=0,
                                                            last_epoch=-1)
 
 # training
-
-nb_cycles = 100
-nb_epochs_self_supervised = 10
-nb_epochs_supervised = 1
 
 for cycles in range (nb_cycles) :
     for epochs in range(nb_epochs_self_supervised) :
