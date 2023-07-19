8880446fdd9a26ea05d858d9cd355adc89b38172
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-19 14:10:53 +0200

    maj

--- dataloader.py
+++ dataloader.py
@@ -2,13 +2,15 @@ import torch
 import torchvision
 
 from transforms import MyTransform, basic_transformation, test_transformation
+from lightly.transforms.simclr_transform import SimCLRTransform
+
 
 batch_size = 512
 
 train_dataset_self_supervised = torchvision.datasets.CIFAR10(
     root='./data_cifar10_train',
     train=True,
-    transform=MyTransform(input_size=32, gaussian_blur=0.5),
+    transform=SimCLRTransform(input_size=32, gaussian_blur=0.5),
     download=True
 )
 
