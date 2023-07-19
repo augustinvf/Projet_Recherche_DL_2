9b7212a7373f8975123e94557e268c1e3fd6c319
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-19 15:58:23 +0200

    maj

--- model.py
+++ model.py
@@ -6,10 +6,9 @@ class Model(nn.Module):
     def __init__(self, projection_head, input_size_classifier, nb_classes):
         super().__init__()
         resnet = torchvision.models.resnet18()
+        resnet.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+        resnet.maxpool = nn.Identity()
         self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-        print(self.backbone.conv1.out_features)
-        #self.backbone.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
-        #self.backbone.maxpool = nn.Identity()
         self.projection_head = projection_head
         self.classifier = nn.Linear(input_size_classifier, nb_classes)
 
