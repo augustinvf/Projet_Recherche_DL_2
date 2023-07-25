8c2f30b200c836e63cf18f758ca19b1de2ad85fb
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-25 16:20:20 +0200

    maj

--- main.py
+++ main.py
@@ -27,28 +27,31 @@ config = omegaconf.OmegaConf.load("config.yaml")
 
 # tool initialization
 
+print(config.scheduler_su.T_max)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 nb_classes = config.nb_classes
-print(nb_classes)
-input_size_classifier = 512
+input_size_classifier = config.input_size_classifier
 projection_head = SimCLRProjectionHead(512, 512, 128)
 nb_steps = len(train_dataloader_supervised)
 
-nb_cycles = 1
-nb_epochs_self_supervised_by_cycle = 100
-nb_epochs_supervised_by_cycle = 100
+nb_cycles = config.nb_cycles
+nb_epochs_self_supervised_by_cycle = config.nb_epochs_self_supervised_by_cycle
+nb_epochs_supervised_by_cycle = config.nb_epochs_supervised_by_cycle
 
 model = Model(projection_head, input_size_classifier, nb_classes).to(device)
 
 criterion_ss = NTXentLoss()
-optimizer_ss = torch.optim.SGD(list(model.backbone.parameters()) + list(model.projection_head.parameters()), 0.2, momentum = 0.9, weight_decay=1e-6)
-scheduler_ss = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_ss, T_max=nb_cycles*nb_epochs_self_supervised_by_cycle, eta_min=0,
+optimizer_ss = torch.optim.SGD(list(model.backbone.parameters()) + list(model.projection_head.parameters()), 
+                               config.optimizer_ss.lr, config.optimizer_ss.momentum, config.optimizer_ss.weight_decay)
+scheduler_ss = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_ss, config.scheduler_ss.T_max, config.scheduler_ss.eta_min,
                                                            last_epoch=-1)
 
 criterion_su = nn.CrossEntropyLoss()
-optimizer_su = torch.optim.SGD(model.classifier.parameters(), lr = 0.01, momentum = 0.9, weight_decay=5e-4)
-scheduler_su = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_su, T_max=nb_cycles*nb_epochs_supervised_by_cycle, eta_min=0,
+optimizer_su = torch.optim.SGD(model.classifier.parameters(), config.optimizer_su.lr, 
+                               config.optimizer_su.momentum, config.optimizer_su.weight_decay)
+scheduler_su = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_su, config.scheduler_su.T_max, config.scheduler_su.eta_min,
                                                            last_epoch=-1)
 
 # training
